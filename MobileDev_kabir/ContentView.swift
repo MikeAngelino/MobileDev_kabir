cbfec3b427ea669ce6773696614a3b55e7d57c3a
DePublicEnemy <kzala001@fiu.edu>
2020-08-21 19:25:35 -0400

    Initial Commit

---
+++ MobileDev_kabir/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MobileDev_kabir
+//
+//  Created by Kabir Zaladin on 8/21/20.
+//  Copyright © 2020 Kabir Zaladin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
